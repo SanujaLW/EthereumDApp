03a316cba252c0fa7ab33779d4a33bf66e8e2203
SanujaLW <sanujalw@gmail.com>
2020-02-16 15:19:08 +0530

    Config uPort service

--- dAppClient/src/app/d-app/services/uport.service.ts
+++ dAppClient/src/app/d-app/services/uport.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Connect } from 'uport-connect';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UportService {
 
-  constructor() { }
+  private _uport: any;
+
+  constructor() {
+    this._uport = new Connect('dAppClient', { network: 'ropsten'});
+   }
 }
